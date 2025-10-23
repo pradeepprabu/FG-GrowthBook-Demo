c28f4145a6531cfccb674aaaf83af45fe6d16e66
Pradeep Prabhu <Pradeep.Prabhu@frasers.group>
2025-10-23 16:29:02 +0100

    clean up env usage

--- src/app/customgrowthbookadapter.ts
+++ src/app/customgrowthbookadapter.ts
@@ -4,10 +4,10 @@ export const myGrowthbookAdapter = createGrowthbookAdapter({
   clientKey: "sdk-8gh1re4DQqWUeLi",
   apiHost: "http://localhost:3100", // optional
   appOrigin: "http://localhost:3000", // optional
-  edgeConfig: {
-    connectionString: process.env.GROWTHBOOK_EDGE_CONNECTION_STRING!,
-    itemKey: process.env.GROWTHBOOK_EDGE_CONFIG_ITEM_KEY, // optional
-  },
+  // edgeConfig: {
+  //   connectionString: process.env.GROWTHBOOK_EDGE_CONNECTION_STRING!,
+  //   itemKey: process.env.GROWTHBOOK_EDGE_CONFIG_ITEM_KEY, // optional
+  // },
   trackingCallback: (experiment, result) => {
     // Back-end exposure logging
   },
